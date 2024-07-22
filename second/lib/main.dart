693da371ce0dd44b529e7ea1aa06e611250c7b62
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-07-22 11:11:50 +0530

    3

--- second/lib/main.dart
+++ second/lib/main.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.grey),
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("AppBar"),
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Multi-Page App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: const Center(child: Text("some text")),
-    ),
-  ));
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to Second Page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go Back to Main Page'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
 }
